e1cb1ab1fb65d9675e031f385c9c7bead9269edd
jacey04 <113055266+jacey04@users.noreply.github.com>
2022-09-16 14:37:05 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.TiltLeft, function () {
+    sprite.move(1)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    sprite.move(1)
+})
+let sprite: game.LedSprite = null
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+	
+})
