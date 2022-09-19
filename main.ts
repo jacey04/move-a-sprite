533c9ff463f60fa1587e1d0318479a6e634f7766
jacey04 <113055266+jacey04@users.noreply.github.com>
2022-09-19 13:54:43 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 input.onGesture(Gesture.TiltLeft, function () {
-    sprite.move(1)
+    sprite.move(-1)
 })
 input.onGesture(Gesture.TiltRight, function () {
     sprite.move(1)
 })
 let sprite: game.LedSprite = null
 basic.showIcon(IconNames.Happy)
+sprite = game.createSprite(2, 2)
 basic.forever(function () {
 	
 })
